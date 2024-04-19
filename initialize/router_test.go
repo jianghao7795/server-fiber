0daf47b5ed2c984e081f960f0fce74845a73f3c3
jianghao <jianghao@haplox.com>
2024-04-19 11:37:05 +0800

    fix: 生成test

---
+++ initialize/router_test.go
@@ -0,0 +1,30 @@
+/*
+ * @Author: jianghao
+ * @Date: 2022-07-29 09:48:24
+ * @LastEditors: jianghao
+ * @LastEditTime: 2022-10-17 11:27:44
+ */
+package initialize
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestRouters(t *testing.T) {
+	tests := []struct {
+		name string
+		want *fiber.App
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Routers(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Routers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ utils/directory_test.go
@@ -0,0 +1,29 @@
+package utils
+
+import "testing"
+
+func TestPathExists(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := PathExists(tt.args.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PathExists() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("PathExists() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
