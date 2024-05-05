09e9f0cce5299a046f2f3655d3a2b7c17a9796b5
jianghao <97561120@qq.com>
2024-05-05 17:30:57 +0800

    feat: 去掉导出文件的操作历史

--- router/example/exa_excel.go
+++ router/example/exa_excel.go
@@ -17,7 +17,6 @@ func (e *ExcelRouter) InitExcelRouter(Router fiber.Router) {
 	excelRouter.Get("getFileInfoList", exaExcelApi.GetFileList)                                   // 获取上传文件成功列表
 	excelRouter.Delete("deleteFile/:id", middleware.OperationRecord, exaExcelApi.DeleteFile)      // 删除文件
 	excelRouter.Get("loadExcel", middleware.OperationRecord, exaExcelApi.LoadExcel)               // 加载Excel数据
-	excelRouter.Post("exportExcel", middleware.OperationRecord, exaExcelApi.ExportExcel)          // 导出Excel
-	excelRouter.Get("downloadTemplate", middleware.OperationRecord, exaExcelApi.DownloadTemplate) // 下载模板文件
-
+	excelRouter.Post("exportExcel", exaExcelApi.ExportExcel)          // 导出Excel
+	excelRouter.Get("downloadTemplate", exaExcelApi.DownloadTemplate) // 下载模板文件
 }
