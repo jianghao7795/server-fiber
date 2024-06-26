9428814d54de9aa0127dc41b3c44259ecb2c8ba6
jianghao <jianghao@haplox.com>
2024-06-26 16:21:42 +0800

    fix: config 添加signing-key

--- config/jwt.go
+++ config/jwt.go
@@ -8,4 +8,5 @@ type JWT struct {
 	ExpiresTime int64           `mapstructure:"expires-time" json:"expires-time" yaml:"expires-time"` // 过期时间
 	BufferTime  int64           `mapstructure:"buffer-time" json:"buffer-time" yaml:"buffer-time"`    // 缓冲时间
 	Issuer      string          `mapstructure:"issuer" json:"issuer" yaml:"issuer"`                   // 签发者
+	SigningKey  string          `mapstructure:"signing-key" json:"signing-key" yaml:"signing-key"`    // 签名密钥 HS256使用
 }
