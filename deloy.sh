818f9188ae1796e8e714219c7b00a04ee14df520
jianghao <jianghao@haplox.com>
2024-11-15 09:07:38 +0800

    feat: 更新编译脚本

--- deloy.sh
+++ deloy.sh
@@ -10,4 +10,4 @@ docker rm fiber
 docker rmi fiber
 
 docker build --progress=plain -t fiber .
-docker run --name fiber -d -v ./log/:/app/log/ -v ./uploads:/app/uploads -p 3100:3100 fiber
+docker run --name fiber -d -v ./logs/:/app/logs/ -v ./uploads:/app/uploads -p 3100:3100 fiber
